3a34f5df42ec19ebeb7f4ad3023e168d6843f198
TanelToe <tanel.toemets1@gmail.com>
2017-03-05 18:31:19 +0200

    Üritasin sisselogimist panna andmebaasiga suhtlema
    ei tööta

--- Fitt_spring/Fitt/src/app.js
+++ Fitt_spring/Fitt/src/app.js
@@ -2,10 +2,10 @@ export class App {
     
 configureRouter(config, router) {
     this.router = router;
-    config.title = 'Fitt 123';
+    config.title = 'Fitt';
     config.map([
       { route: ['', 'home'],       name: 'home',       moduleId: 'home/index', title:"Esileht", nav: true },
-      { route: 'exercise', name: 'exercise', moduleId: 'exercise/exercise',title:"Exercise", nav: true },
+      { route: 'signup', name: 'signup', moduleId: 'signup/signup',title:"Signup", nav: true },
     ]);
   }
 }
---
+++ Fitt_spring/Fitt/src/signup/signup.js
@@ -0,0 +1,26 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
+export class signup{
+    
+    userData = {}
+    
+    constructor(){
+        this.appName = "Fitt"
+        this.count = 9
+    }
+    
+    addUser(){
+        let client = new HttpClient();
+        
+        client.fetch('http://localhost:8080/users/add', {
+            'method': "POST",
+            'body': json(this.userData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Server saatis " + data.firstName);
+        });
+        console.log("Method executed!")
+        
+    }
+}
