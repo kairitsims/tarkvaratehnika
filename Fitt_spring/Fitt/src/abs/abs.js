ab781a3aae2140e494f7339b90842004583bc922
Tanel123 <tanel.toemets1@gmail.com>
2017-04-04 15:24:11 +0300

    arms jt lehed lisavad tehtud treenigud anmebaasi

--- Fitt_spring/Fitt/src/abs/abs.js
+++ Fitt_spring/Fitt/src/abs/abs.js
@@ -1,19 +1,56 @@
 import {HttpClient, json} from 'aurelia-fetch-client'
+import {AureliaCookie} from 'aurelia-cookie'
 
-export class soojendus{
+export class abs{
+    
+    
+    millisToMinutesAndSeconds(millis){
+      var minutes = Math.floor(millis / 60000);
+      var seconds = ((millis % 60000) / 1000).toFixed(0);
+      return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
+    
+    addTraining(){
+        let newTime = new Date();
+        let newTime2 = newTime.getTime();
+        var time2 = AureliaCookie.get('time');
+        let time1 = newTime2 - time2
+        var finaltime = this.millisToMinutesAndSeconds(time1) + " minutit";
+        console.log(finaltime)
+        
+        let client = new HttpClient();
+        
+        client.fetch('http://localhost:8080/training/add', {
+            'method': "POST",
+            'body': json({ "username": AureliaCookie.get('username'),
+                         "trainingName": "Kõhulihaste treening",
+                         "trainingComments": this.trainingData.trainingComments,
+                         "trainingLength": finaltime })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Töötab");
+        });
+        document.getElementById("form").reset();
+    }
     
     exerciseList= []
     
     activate(){
+        
+        var aeg = new Date();
+        var aeg2 = aeg.getTime();
+        AureliaCookie.set('time', aeg2, {
+            expiry: 1, // Expiry in hours, -1 for never expires or minimum 1 for one hour, 2 for two hours and so
+            path: '', // Specify cookie path
+            domain: '', // Domain restricted cookie
+            secure: false // Either true or false
+        });
         
         let client = new HttpClient();
         
         client.fetch('http://localhost:8080/exercise/abs')
             .then(response => response.json())
             .then(exercises => this.exerciseList = exercises);
-
-        
-    }
-
-    
+    }  
 }
