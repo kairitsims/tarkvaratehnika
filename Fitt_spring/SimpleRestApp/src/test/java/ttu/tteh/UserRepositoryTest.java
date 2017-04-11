f1182c34b022fd84546c986700685bb838c3b9ee
Tanel123 <tanel.toemets1@gmail.com>
2017-04-11 14:40:49 +0300

    Unit testid
    Probleem: kuidas teha?

---
+++ Fitt_spring/SimpleRestApp/src/test/java/ttu/tteh/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package ttu.tteh;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserRepositoryTest {
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+}
